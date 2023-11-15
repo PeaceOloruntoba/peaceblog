3800478ca7a438c77dce2ba4d5e6dfb76fd94454
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-11-15 11:08:20 +0100

    vgbhnm,

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        dark: "#1b1b1b",
+        light: "#fff",
+        accent: "#7b00d3",
+        accentDark: "#ffdb4d",
+        gray: "#747474",
       },
+      fontFamily: {
+        mr: ["var(--font-mr)"],
+        in: ["var(--font-in)"]
+      }
     },
   },
   plugins: [],

49f606ebac22cbeff2fba836876c7aee522a0b6f
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-11-16 13:22:55 +0100

    xcvbn

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import AboutCoverSection from '@/components/About/AboutCoverSection'
-import Image from 'next/image'
+import {allBlogs} from "contentlayer/generated";
+import HomeCoverSection from "../components/Home/HomeCoverSection";
+import FeaturedPosts from "../components/Home/FeaturedPosts";
+import RecentPosts from "../components/Home/RecentPosts";
+
 
 export default function Home() {
   return (
     <div className='flex items-center font-bold text-5xl justify-center p-96'>
-      Hello World
-      <AboutCoverSection />
+      <main className="flex flex-col items-center justify-center">
+      <HomeCoverSection blogs={allBlogs} />
+      <FeaturedPosts blogs={allBlogs} />
+      <RecentPosts blogs={allBlogs} />
+
+
+    </main>
     </div>
   )
 }

2c765ed42f124250187a8b1902c68b8d10e41b40
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-11-16 12:59:58 +0100

    nm,n

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import AboutCoverSection from '@/components/About/AboutCoverSection'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className='flex items-center font-bold text-5xl justify-center p-96'>
       Hello World
+      <AboutCoverSection />
     </div>
   )
 }
